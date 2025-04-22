6d13403166ffd85d72ca37c5bb26254f2281eafa
Racksomware <kur01nekogm@gmail.com>
2025-04-22 09:47:58 +0200

    first push
    ugh, no time to decor this.
    just a small, quick, and dirty push, to be able to later on continue at home.

---
+++ underscoreunicode/code.lua
@@ -0,0 +1,30 @@
+--[[
+  NOTA DE DESARROLLO:
+  Este juego adopta un estilo de programacion orientada a objetos (O minimo, una parecida a).
+  SI por alguna razon se busca modificar datos sobre un objeto, busca el archivo con el nombre del objeto,
+  NO edites directamente en "code.lua"!
+]]--
+
+function Init()
+
+end
+
+--[[
+  The Update() method is part of the game's life cycle. The engine calls
+  Update() on every frame before the Draw() method. It accepts one argument,
+  timeDelta, which is the difference in milliseconds since the last frame.
+]]--
+function Update(timeDelta)
+
+end
+
+--[[
+  The Draw() method is part of the game's life cycle. It is called after
+  Update() and is where all of our draw calls should go. We'll be using this
+  to render sprites to the display.
+]]--
+function Draw()
+  DrawMetaSprite(dummytest,32,32)
+  DrawRect(32,32,1,1,5,5)
+  RedrawDisplay()
+end
---
+++ underscoreunicode/objectx/player.lua
@@ -0,0 +1,14 @@
+--[[
+No y ps, datos del objeto que el jugador adopta.
+
+Aquí el tema es, aparte de indicar el tema del salto y todo eso, no chequea mucho mas,
+pues simplemente sirve como un metodo de generacion multiple para este jugador.
+]]--
+
+-- Primero se indica variables "fijas" del jugador.
+--Todo aquí esta medido a base de pixeles/segundo.
+local P1_VelH_max = 25 --Velocidad de movimiento horizontal maximo.
+local P1_ColBox_H = 14 --Tamaño de la caja de colisiones en Horizontal.
+local P1_ColBox_V = 31 --Tamaño de la caja de colisiones en Vertical.
+local P1_PivPot_H = 8  --Ubicacion (relativa a 0,0 [Arriba Izquierda]) donde el eje principal de colision se encuentra en Horizontal.
+local P1_PivPot_V = 32 --Ubicacion (relativa a 0,0 [Arriba Izquierda]) donde el eje principal de colision se encuentra en Vertical.
